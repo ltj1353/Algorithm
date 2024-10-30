271be1941c15bc92cde7030b7d81e8b45ff7d956
ltj1353 <152738724+ltj1353@users.noreply.github.com>
2024-10-30 13:25:03 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 364 ms, Memory: 269836 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+a, b, c = map(int, input().rstrip().split(' '))
+d = [[] * (a + 1) for i in range(a + 1)]
+
+start = 0
+global visited
+visited = [0] * (a + 1)
+
+for i in range(b):
+    e, f = map(int, input().rstrip().split(' '))
+    d[e].append(f)
+    d[f].append(e)
+
+for i in range(1, a + 1):
+    d[i] = sorted(d[i])
+
+global count
+count = 1
+visit = deque()
+
+def bfs(start):
+    global count
+    global visited
+    visited[start] = count
+    count += 1
+    visit.appendleft(start)
+    while(visit):
+        tmp = visit[-1]
+        visit.pop()
+        for i in d[tmp]:
+            if(visited[i] == 0):
+                visited[i] = count
+                count += 1
+                visit.appendleft(i)
+bfs(c)
+for i in range(1, a + 1):
+    sys.stdout.write(str(visited[i])+'\n')
