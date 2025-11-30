9d5680caaf81484974f9b1911128e2731cc0dff7
ltj1353 <152738724+ltj1353@users.noreply.github.com>
2025-11-30 12:36:53 +0900

    [Platinum IV] Title: Pilots, Time: 1700 ms, Memory: 37836 KB -BaekjoonHub

--- 백준/Platinum/8201. Pilots/Pilots.cc
+++ 백준/Platinum/8201. Pilots/Pilots.cc
@@ -2,41 +2,41 @@
 #include <deque>
 using namespace std;
 
-int n;
 long long a;
+int b;
 long long c[3000000];
 
 int main() {
     
-    cin >> a >> n;
-    for (int i = 0; i < n; i++) cin >> c[i];
+    cin >> a >> b;
+    for (int i = 0; i < b; i++)
+        cin >> c[i];
 
-    deque<int> maxDeque; // 감소 덱, 윈도우 최대값
-    deque<int> minDeque; // 증가 덱, 윈도우 최소값
+    deque<int> maxi;
+    deque<int> mini;
 
     int fir = 0;
     long long ans = 0;
 
-    for (int las = 0; las < n; las++) {
-        // 최대값 덱 갱신
-        while (!maxDeque.empty() && c[maxDeque.back()] <= c[las])
-            maxDeque.pop_back();
-        maxDeque.push_back(las);
+    for (int las = 0; las < b; las++) {
+        while (!maxi.empty() && c[maxi.back()] <= c[las])
+            maxi.pop_back();
+        maxi.push_back(las);
 
-        // 최소값 덱 갱신
-        while (!minDeque.empty() && c[minDeque.back()] >= c[las])
-            minDeque.pop_back();
-        minDeque.push_back(las);
+        while (!mini.empty() && c[mini.back()] >= c[las])
+            mini.pop_back();
+        mini.push_back(las);
 
-        // 슬라이딩 윈도우 조건 체크: max - min > a
-        while (!maxDeque.empty() && !minDeque.empty() && c[maxDeque.front()] - c[minDeque.front()] > a) {
-            // 왼쪽 포인터 이동
-            if (maxDeque.front() == fir) maxDeque.pop_front();
-            if (minDeque.front() == fir) minDeque.pop_front();
+        //cout << maxi[0] << mini[0] << endl;
+
+        while (c[maxi[0]] - c[mini[0]] > a) {
+            if (maxi[0] == fir)
+                maxi.pop_front();
+            if (mini[0] == fir)
+                mini.pop_front();
+            //cout << fir << las << endl;
             fir++;
         }
-
-        // 현재 윈도우 길이 갱신
         ans = max(ans, (long long)(las - fir + 1));
     }
 
