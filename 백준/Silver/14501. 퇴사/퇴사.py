b23b740dcacfc692600178771a9ad10a8eae8c81
ltj1353 <152738724+ltj1353@users.noreply.github.com>
2025-12-05 14:53:47 +0900

    [Silver III] Title: 퇴사, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,19 @@
+#확인용입니다
+import sys
+input = sys.stdin.readline
+
+a = int(input())
+b = [[0, 0]] + [list(map(int, input().split())) for _ in range(a)]
+
+c = [0] * (a + 2)
+
+for i in range(a, 0, -1):
+    t = b[i][0]
+    p = b[i][1]
+    f = i + t
+    if f > a + 1:
+        c[i] = c[i + 1]
+    else:
+        c[i] = max(c[i + 1], p + c[f])
+
+print(c[1])
